b284e3ca5adfdaca431a6f1a790fcf95f6497bca
aditisinghal651 <aditisinghal121@gmail.com>
2022-10-01 20:33:14 +0530

    Updated folder

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -8,10 +8,19 @@ public class BinaryToDecimal {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        decimalNum(n);
+        System.out.println(decimalNum(n));
     }
 
-    private static void decimalNum(int n) {
+    private static int decimalNum(int n) {
+        int pv = 1;
+        int ans = 0;
 
+        while(n > 0){
+            int rem = n % 10;
+            n = n / 10;
+            ans = ans + (rem*pv);
+            pv = pv * 2;
+        }
+        return ans;
     }
 }
