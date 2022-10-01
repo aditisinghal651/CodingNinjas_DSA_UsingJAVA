cfc1ec5f8e912881b51a2fba4ff2f7f6c5ecdfba
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-01 20:55:10 +0530

    updated folder

---
+++ DecimalToBinary.java
@@ -0,0 +1,17 @@
+package IntroductionToJava;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        binaryNum(n);
+    }
+
+    private static int binaryNum(int n) {
+        
+    }
+}
