016f7343c66cf378a1734a3913a3b54da73b5ed4
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-07 13:21:40 +0530

    MirrorImageNumber completed

--- Pattern2/MirrorImageNumber.java
+++ Pattern2/MirrorImageNumber.java
@@ -12,6 +12,19 @@ public class MirrorImageNumber {
     }
 
     private static void mirrorImage(int n) {
-
+        for(int i = 1; i <= n; i++){
+            int j = 1;
+            while(j <= n - i){
+                System.out.print(" ");
+                j++;
+            }
+            j = 1;
+            int startingNum = 1;
+            while(j <= i){
+                System.out.print(startingNum++);
+                j++;
+            }
+            System.out.println();
+        }
     }
 }
