87f940af1a95718fc37235834d3c5303dcedf527
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-07 13:01:12 +0530

    Armstrong function Body created

---
+++ Test1/CheckArmstrong.java
@@ -0,0 +1,17 @@
+package IntroductionToJava.Test1;
+
+import java.util.Scanner;
+
+public class CheckArmstrong {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        armstrong(n);
+    }
+
+    private static void armstrong(int n) {
+
+    }
+}
