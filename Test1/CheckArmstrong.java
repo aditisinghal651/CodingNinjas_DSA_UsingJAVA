e5ccab4690665b1e1ea0bbc1867304dff8bd9b08
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-09 14:53:24 +0530

    Check the armstrong number

--- Test1/CheckArmstrong.java
+++ Test1/CheckArmstrong.java
@@ -1,17 +1,38 @@
 package IntroductionToJava.Test1;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.util.Scanner;
 
 public class CheckArmstrong {
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int num = sc.nextInt();
 
-        armstrong(n);
+        System.out.println(armstrong(num));
     }
 
-    private static void armstrong(int n) {
+    private static boolean armstrong(int num) {
+        int n = 0;
+        int temp = num;
+        while(temp > 0){
+            //int rem = num%10;
+            n++;
+            temp = temp/10;
+        }
+        int number = num;
+        int sum = 0;
+        while(num > 0){
+            int rem = num%10;
+            sum += Math.pow(rem, n);
+            num = num/10;
+        }
 
+        if(sum == number){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
