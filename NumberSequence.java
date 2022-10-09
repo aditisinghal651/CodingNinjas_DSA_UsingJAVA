2b2685e2332186e59db230564490ef5292f7db84
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-09 10:55:03 +0530

    NumberSequence checked using ArrayInput

--- NumberSequence.java
+++ NumberSequence.java
@@ -4,19 +4,34 @@ import java.util.Scanner;
 
 public class NumberSequence {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        checkSequence(arr);
+        System.out.println(checkSequence(arr));
     }
 
     private static boolean checkSequence(int[] arr) {
+        boolean isDec = true;
 
+        for (int i = 1; i < arr.length; i++) {
+            int prev = arr[i - 1];
+            int curr = arr[i];
+
+            if (prev == curr) {
+                return false;
+            }
+            if (isDec && curr > prev) {
+                isDec = false;
+            } else if (!isDec && curr < prev) {
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ Pattern2/StarPyramidPattern.java
@@ -0,0 +1,17 @@
+package IntroductionToJava.Pattern2;
+
+import java.util.Scanner;
+
+public class StarPyramidPattern {
+
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        StarPyramid(n);
+    }
+
+    private static void StarPyramid(int n) {
+        
+    }
+}
