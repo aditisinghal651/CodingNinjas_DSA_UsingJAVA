2d28fb4b0d4f4b50e415d6013cbb72647f105ce2
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-10 14:38:53 +0530

    arrange Number body created

---
+++ Arrays/ArrangeNumbers.java
@@ -0,0 +1,23 @@
+package IntroductionToJava.Arrays;
+
+import java.util.Scanner;
+
+public class ArrangeNumbers {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int n = 0;
+
+        for(int i = 1; i <= t; i++){
+            n = sc.nextInt();
+        }
+
+        int[] arr = new int[n];
+        arrangeNum(arr, n);
+    }
+
+    private static void arrangeNum(int[] arr, int n) {
+
+    }
+}
