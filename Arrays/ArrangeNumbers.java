2517b319b1d2ab9caeb9c1751f1d3e8a42388451
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-10 14:41:16 +0530

    arrange Number body updated

--- Arrays/ArrangeNumbers.java
+++ Arrays/ArrangeNumbers.java
@@ -7,14 +7,13 @@ public class ArrangeNumbers {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        int n = 0;
+        int n;
 
         for(int i = 1; i <= t; i++){
             n = sc.nextInt();
+            int[] arr = new int[n];
+            arrangeNum(arr, n);
         }
-
-        int[] arr = new int[n];
-        arrangeNum(arr, n);
     }
 
     private static void arrangeNum(int[] arr, int n) {
