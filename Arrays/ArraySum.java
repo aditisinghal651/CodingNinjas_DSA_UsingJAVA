a69a36b44eaf98ed98db31c79b3cddb1b51bb6a7
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-10 10:40:39 +0530

    enhanced for loop used in array elements sum

--- Arrays/ArraySum.java
+++ Arrays/ArraySum.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class ArraySum {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         arrayInput();
     }
 
-    private static void arrayInput() {
+    public static void arrayInput() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of Test Cases:");
         int t = sc.nextInt();
@@ -27,8 +27,12 @@ public class ArraySum {
 
     private static void arraySum(int[] arr) {
         int sum = 0;
-        for(int i = 0; i < arr.length; i++){
-            sum = sum + arr[i];
+//        for(int i = 0; i < arr.length; i++){
+//            sum = sum + arr[i];
+//        }
+        // enhanced for loop
+        for(int number : arr){
+            sum += number;
         }
         System.out.println(sum);
     }
