49336f574cf8081b29f9ba76f88e83c03e93ca04
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-10-23 13:04:34 +0530

    method 1 for sorting 0 1: when 2 scans of array allowed

--- Arrays/SortZeroOne.java
+++ Arrays/SortZeroOne.java
@@ -11,23 +11,28 @@ public class SortZeroOne {
 
     private static int[] sorting(int[] arr) {
         //Method - 1 : When one can do 2 scans of the array
-        int count = 0;
-        for (int i = 0; i < arr.length; i++){
-            if(arr[i] == 0){
-                count++;
-            }
-        }
-        for(int i = 0; i < count; i++){
-            arr[i] = 0;
-        }
-        for(int j = count; j < arr.length; j++){
-            arr[j] = 1;
-        }
-        return arr;
+//        int count = 0;
+//        for (int i = 0; i < arr.length; i++){
+//            if(arr[i] == 0){
+//                count++;
+//            }
+//        }
+//        for(int i = 0; i < count; i++){
+//            arr[i] = 0;
+//        }
+//        for(int j = count; j < arr.length; j++){
+//            arr[j] = 1;
+//        }
+//        return arr;
 
         //Method - 2 : When only 1 scan of the arrays is allowed
+        int i = 0;
+        int j = arr.length;
+
+        while(i < j){
 
 
-
+        }
+        return arr;
     }
 }
