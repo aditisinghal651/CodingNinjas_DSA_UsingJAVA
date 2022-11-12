08781438e6763c28a34ff6e686fc375e46580419
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-11-12 20:00:06 +0530

    Star Pyramid Pattern

--- Pattern2/StarPyramidPattern.java
+++ Pattern2/StarPyramidPattern.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class StarPyramidPattern {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
@@ -12,6 +12,15 @@ public class StarPyramidPattern {
     }
 
     private static void StarPyramid(int n) {
-        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 1; k <= ((2*i)-1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
