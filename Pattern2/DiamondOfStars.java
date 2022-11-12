500d1e8f79e14dea0ba4030af53acc64e2eaa109
aditisinghal651 <iamaditisinghal25@gmail.com>
2022-11-12 23:12:00 +0530

    star Diamond pattern problem

---
+++ Pattern2/DiamondOfStars.java
@@ -0,0 +1,18 @@
+package IntroductionToJava.Pattern2;
+
+import java.util.Scanner;
+
+public class DiamondOfStars {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        starDiamond(n);
+    }
+
+    private static void starDiamond(int n) {
+        for(int i = 1; i <= n; i++){
+            
+        }
+    }
+}
